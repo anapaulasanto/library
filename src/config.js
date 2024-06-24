ba8133fd6523fb81ea7417b54dee6b981f92f142
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2024-06-24 20:20:31 -0300

    update uri

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const API_URL = 'llibrary-backend-production.up.railway.app'; // Substitua pela URL do seu backend no Railway
+const API_URL = 'library-production-60c8.up.railway.app'; // Substitua pela URL do seu backend no Railway
 export default API_URL;
